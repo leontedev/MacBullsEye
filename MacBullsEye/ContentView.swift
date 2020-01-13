92d9508a00849d8d4bd085349bd48c4466f268af
Mihai Leonte <leontemihai@gmail.com>
2020-01-13 11:31:36 +0200

    Project setup

---
+++ MacBullsEye/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacBullsEye
+//
+//  Created by Mihai Leonte on 13/01/2020.
+//  Copyright © 2020 Mihai Leonte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
